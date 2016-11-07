4a4619eac775a40d5741c382cef452a3ef2a5b67
陈真 <361914037@163.com>
2016-11-08 01:03:49 +0800

    使用ES6中的class

---
+++ src/dog.js
@@ -0,0 +1,11 @@
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+
+    bark() {
+        return `Wah wah, I am ${this.name}`;
+    }
+}
+
+module.exports = Dog;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-const str = 'ES6';
-console.log(`Hello ${str}`);
+const Dog = require('./dog');
+
+const toby = new Dog('Toby');
+
+console.log(toby.bark());
